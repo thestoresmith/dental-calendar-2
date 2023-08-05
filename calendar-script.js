997d33c3f21c81adddff950d6d54d763d80430fd
thestoresmith <jmschmid@gmail.com>
2023-08-05 17:19:37 -0400

    Added colors to dates for static demo

--- calendar-script.js
+++ calendar-script.js
@@ -8,16 +8,60 @@ const augustDays = [
     [24, 25, 26, 27, 28, 29, 30]
 ];
 
-// Get the calendar grid body
-const calendarBody = document.querySelector('#calendar-grid tbody');
+const dayColors = [
+    0,0,0,0,0,0,0,
+    0,0,0,0,1,1,0,
+    0,2,3,3,3,3,0,
+    0,3,3,3,3,3,0,
+    0,3,1,1,4,0,0,
+    0,0,0,0,0,0,0,
+]
 
-// Populate the calendar grid with days
-for (const week of augustDays) {
+// Get the calendar table body
+const calendarBody = document.querySelector('#calendar-table tbody');
+
+// Populate the calendar table with days and apply colors
+for (let i = 0; i < augustDays.length; i++) {
+    const week = augustDays[i];
+    const colorIndices = dayColors.slice(i * 7, (i + 1) * 7); // Get color indices for the week
+
     const row = document.createElement('tr');
-    for (const day of week) {
+    for (let j = 0; j < week.length; j++) {
+        const day = week[j];
         const cell = document.createElement('td');
         cell.textContent = day;
+
+        // Apply background color based on color index using CSS variables
+        if (colorIndices[j] === 0) {
+            cell.style.backgroundColor = ''; // No color
+        } else if (colorIndices[j] === 1) {
+            cell.style.backgroundColor = 'var(--shipping-day-color)'; // Shipping day color
+        } else if (colorIndices[j] === 3) {
+            cell.style.backgroundColor = 'var(--in-lab-day-color)'; // In-lab day color
+        } else if (colorIndices[j] === 2) {
+            cell.style.backgroundColor = 'var(--holiday-day-color)'; // Holiday color
+        } else if (colorIndices[j] === 4) {
+            cell.style.backgroundColor = 'var(--delivery-day-color)'; // Delivery day color
+        }
+
         row.appendChild(cell);
     }
     calendarBody.appendChild(row);
+}
+
+// Get the element that displays the month name
+const monthNameElement = document.querySelector('#month-name-row th');
+
+// Update the month name
+monthNameElement.textContent = 'August';
+
+// Assume you want to change the color of the date "31" (for example)
+const specificDate = document.querySelector('#calendar-table td:contains("31")');
+
+// Get the value of the custom property for shipping day color
+const shippingDayColor = getComputedStyle(document.documentElement).getPropertyValue('--shipping-day-color');
+
+// Update the color of the specific date
+if (specificDate) {
+    specificDate.style.backgroundColor = shippingDayColor;
 }
