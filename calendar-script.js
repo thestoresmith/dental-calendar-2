6e7d33e6a4eb9536efd5a533cd51e71d3bf236ca
thestoresmith <jmschmid@gmail.com>
2023-08-06 23:46:33 -0400

    js test

--- calendar-script.js
+++ calendar-script.js
@@ -1,11 +1,26 @@
-const shippingForm = document.getElementById('shipping-form');
-const shippingDateInput = document.getElementById('shipping-date');
-const productTypeSelect = document.getElementById('product-type');
-shippingForm.addEventListener('submit', function (event) {    
-    
-    const calendarContainer = document.getElementById('calendar-table-container');
-    const legendContainer = document.getElementById('calendar-legend-container');
-    calendarContainer.style.display = 'block';
-    legendContainer.style.display = 'flex';
+document.addEventListener("DOMContentLoaded", function () {
+    const calendarTableContainer = document.getElementById("calendar-table-container");
+    const calendarLegendContainer = document.getElementById("calendar-legend-container");
+    const submitButton = document.getElementById("submit-button");
+  
+    calendarTableContainer.style.display = "none";
+    calendarLegendContainer.style.display = "none";
+  
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault()
+        const shippingDateInput = document.getElementById('shipping-date');
+        const productTypeSelect = document.getElementById('product-type');
+        if (!shippingDateInput.value || productTypeSelect.value === '') {
+            // Display an error message to the user (you can modify this part)
+            alert('Please select a date and a product.');
 
-})
+            // Highlight the invalid fields
+            if (!shippingDateInput.value) {
+                shippingDateInput.classList.add('invalid');
+            }
+            if (productTypeSelect.value === '') {
+                productTypeSelect.classList.add('invalid');
+            }
+        } else {  
+    });
+});
