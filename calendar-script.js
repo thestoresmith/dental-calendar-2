6fbda41aa4b009f30a7f098972da6e01ccb5e3c7
thestoresmith <jmschmid@gmail.com>
2023-08-07 03:03:10 -0400

    calendar populate

--- calendar-script.js
+++ calendar-script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const calendarTableContainer = document.getElementById("calendar-table-container");
     const calendarLegendContainer = document.getElementById("calendar-legend-container");
     const submitButton = document.getElementById("submit-button");
-  
     calendarTableContainer.style.display = "none";
     calendarLegendContainer.style.display = "none";
   
@@ -22,8 +21,33 @@ document.addEventListener("DOMContentLoaded", function () {
                 productTypeSelect.classList.add('invalid');
             }
         } else {  
+            // Update the month name based on the selected date
+            const monthNameElement = document.querySelector('#month-name-row th');
+                        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            const selectedDate = new Date(shippingDateInput.value);
+            monthNameElement.textContent = monthNames[selectedDate.getMonth()];
+
+            // Determine the calendar start date
+            const dayOfWeek = selectedDate.getDay();
+            let calendarIndexDate = new Date();
+            calendarIndexDate.setTime(selectedDate.getTime() - (7 + dayOfWeek)*1000*60*60*24);
+            
+            // populate the calendar
+            const calendarBody = document.querySelector('#calendar-table tbody');
+            for (let i = 0; i < 6; i++) {
+                const row = document.createElement('tr');
+                for (let j = 0; j < 7; j++) {
+                    const cell = document.createElement('td');
+                    cell.textContent = calendarIndexDate.getDate();
+                    row.appendChild(cell);
+                    // increment the calendarIndexDate var
+                    calendarIndexDate.setTime(calendarIndexDate.getTime() + 1000*60*60*24);
+                }
+                calendarBody.appendChild(row);
+            }
+            // make the calendar and legend visible
+            calendarTableContainer.style.display = "block";
+            calendarLegendContainer.style.display = "flex";
         }
-        calendarTableContainer.style.display = "block";
-        calendarLegendContainer.style.display = "flex";
     });
 });
