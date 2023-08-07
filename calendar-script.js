8f5e20f38481faef2c08420c245917aede06d424
thestoresmith <jmschmid@gmail.com>
2023-08-06 22:24:42 -0400

    more js testing

---
+++ calendar-script.js
@@ -0,0 +1,11 @@
+const shippingForm = document.getElementById('shipping-form');
+const shippingDateInput = document.getElementById('shipping-date');
+const productTypeSelect = document.getElementById('product-type');
+shippingForm.addEventListener('submit', function (event) {    
+    
+    const calendarContainer = document.getElementById('calendar-table-container');
+    const legendContainer = document.getElementById('calendar-legend-container');
+    calendarContainer.style.display = 'block';
+    legendContainer.style.display = 'flex';
+
+}
